097f5a4e5cf4ca2b15aa24cf02f5aa754a5e26ab
Akash3951 <akashgupta993161@gmail.com>
2023-04-08 14:38:27 +0530

    changes in about section

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,9 @@ export default function About() {
     <div className="about-section">
       <h2 className="about-heading">About</h2>
       <p className="about--details">
-        Hello, my name is Akash, and I am a frontend developer. I enjoy creating
-        visually appealing websites that deliver seamless user experiences.
+        {/* Hello, my name is Akash, and I am a frontend developer. I enjoy creating
+        visually appealing websites that deliver seamless user experiences. */}
+        Hi there! I'm a Full-Stack Engineer with a passion for building innovative and impactful software solutions.  I've honed my skills in coding, testing, and deploying applications that are both user-friendly and efficient.
       </p>
     </div>
   );
